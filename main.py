575beb4d02df17877eeb0fd6556474d83f55b898
Mark Dhas <mark.dhas@gmail.com>
2025-02-27 11:19:39 -0500

    Added the ability to run this from my test box

--- main.py
+++ main.py
@@ -210,5 +210,5 @@ def home():
     return render_template_string(html_template, star_systems=star_systems, universe_totals_text=universe_totals_text)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
